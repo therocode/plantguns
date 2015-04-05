bb97ff6fec86204a1f7daa512fa48b67454a4d66
Tobias Widlund <widlundtobias@gmail.com>
2015-04-05 14:00:56 +0200

    Pathfinding added

--- src/enemies.cpp
+++ src/enemies.cpp
@@ -16,14 +16,21 @@ int32_t Enemy::damage() const
 
 void Enemy::update(Player* player, PlantMap& plants)
 {
+    if(player || plants.size())
+    {
+        mTarget = getTarget(player, plants);
+        Entity::update();
+    }
+}
+
+glm::vec2 Enemy::getTarget(Player* player, PlantMap& plants)
+{
     std::vector<glm::vec2> targets = targetList(player, plants);
 
     if(targets.size() > 0)
-        mTarget = closest(targets);
+        return closest(targets);
     else
-        mTarget = position();
-
-    Entity::update();
+        return center();
 }
 
 std::vector<glm::vec2> Enemy::targetList(Player* player, PlantMap& plants) const
@@ -60,7 +67,8 @@ glm::vec2 Enemy::closest(const std::vector<glm::vec2>& targets) const
     return result;
 }
 
-Spikey::Spikey()
+Spikey::Spikey():
+    mUsePathFinding(false)
 {
     mQuad.setSize({16.0f, 16.0f});
     mHealth = 15;
@@ -75,4 +83,32 @@ void Spikey::update(Player* player, PlantMap& plants)
     Accelerator accelerator;
 
     mAcceleration = accelerator.get(mTarget - center(), 2.0f, mVelocity, 0.1f);
+}
+
+bool Spikey::handleCollision()
+{
+    mUsePathFinding = true;
+    return true;
+}
+
+glm::vec2 Spikey::getTarget(Player* player, PlantMap& plants)
+{
+    if(!mUsePathFinding)
+    {
+        return Enemy::getTarget(player, plants);
+    }
+    else
+    {
+        glm::uvec2 closestTile = (glm::uvec2)(closest(targetList(player, plants)) / 32.0f);
+        glm::uvec2 start = (glm::uvec2)(center() / 32.0f);
+        
+        TilePathAdaptor adaptor(*mCollisionTiles);
+
+        auto path = mPathfinder.findPath(adaptor, start, closestTile);
+
+        if(path.size() > 1)
+            return (glm::vec2)(path[1]) * 32.0f + glm::vec2(16.0f, 16.0f);
+        else 
+            return (glm::vec2)(closestTile) * 32.0f + glm::vec2(16.0f, 16.0f);
+    }
 }
